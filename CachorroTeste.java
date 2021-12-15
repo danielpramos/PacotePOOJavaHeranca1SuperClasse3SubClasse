552c5992aba7d45a2bbb22eb7c2591e03ed0ca96
Daniel Pinheiro Ramos <daniel.ramos5291@gmail.com>
2021-12-15 17:24:41 -0300

    SubClasse Cachorro Main

---
+++ CachorroTeste.java
@@ -0,0 +1,28 @@
+package PacotePOOJavaHeranca;
+
+public class CachorroTeste 
+{
+
+	public static void main(String[] args) 
+	{
+		Cachorro cachorro1 = new Cachorro("cachorro", "mamífero", "casa das pessoas","médio", "São Bernardo", 
+				"bege", "grande");
+		
+		 cachorro1.cachorroInfo();
+		 cachorro1.setAlterarAcao();
+		 cachorro1.setAndar();
+		 cachorro1.setAlterarAcao();
+		 cachorro1.setLatir();
+		 cachorro1.setAlterarAcao();
+		 cachorro1.setPular();
+		 cachorro1.setAlterarAcao();
+		 cachorro1.setRosnar();
+		 cachorro1.setAlterarAcao();
+		 cachorro1.setDormir();
+		 
+		 
+		
+
+	}
+
+}
